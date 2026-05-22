d1dbbcaa97ad6d634471d5f03bcbffc332d52cb4
Kolyunya <OleynikovNY@mail.ru>
2026-05-23 00:25:39 +0300

    Update executor tests

--- tests/timer/callback/test_executor.py
+++ tests/timer/callback/test_executor.py
@@ -1,20 +1,40 @@
 from asyncio import sleep
-from unittest.mock import Mock
+from unittest.mock import AsyncMock, Mock
 
 from pytest import mark
 
-from aiotimer.callback import AsyncExecutor
+from aiotimer.callback import AsyncExecutor, Callback, SyncExecutor
 
 
 @mark.asyncio
-async def test_executor_executes_a_callback() -> None:
+async def test_sync_executor_executes_a_callback() -> None:
     # Arrange
     callback = Mock()
-    executor = AsyncExecutor(Mock(), Mock())
+    executor = SyncExecutor(
+        error_handler=Mock(),
+        error_factory=Mock(),
+    )
 
     # Act
-    await executor(callback, Mock())
-    await sleep(1)
+    await executor(Callback(callback), Mock())
+    await sleep(0.1)
 
     # Assert
     callback.assert_called_once()
+
+
+@mark.asyncio
+async def test_async_executor_executes_a_callback() -> None:
+    # Arrange
+    callback = AsyncMock()
+    executor = AsyncExecutor(
+        error_handler=Mock(),
+        error_factory=Mock(),
+    )
+
+    # Act
+    await executor(Callback(callback), Mock())
+    await sleep(0.1)
+
+    # Assert
+    callback.assert_awaited_once()
