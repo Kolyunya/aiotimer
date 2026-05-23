5c2526ed2ec487d0c57aab5d3085269aacadd927
Kolyunya <OleynikovNY@mail.ru>
2026-05-23 21:23:49 +0300

    Refactor time utility tests

--- tests/tests/utility/time/test_ns2s.py
+++ tests/tests/utility/time/test_ns2s.py
@@ -1,22 +1,14 @@
 from pytest import mark
 
-from aiotimer.utility.time import s2ns
+from aiotimer.utility.time import ns2s
 
 
-@mark.parametrize(('seconds', 'nano_seconds_expected'), [
-    (0.0, 0),
-    (1.0, 1_000_000_000),
-    (42.123_456_789, 42_123_456_789),
+@mark.parametrize(('nanoseconds', 'seconds_expected'), [
+    (0, 0.0),
+    (1_000_000_000, 1.0),
+    (42_123_456_789, 42.123_456_789),
 ])
-def test_s2ns(seconds: float, nano_seconds_expected: int) -> None:
-    nano_seconds = s2ns(seconds)
+def test_ns2s(nanoseconds: int, seconds_expected: float) -> None:
+    seconds = ns2s(nanoseconds)
 
-    assert nano_seconds_expected == nano_seconds
-
-
-def test_s2ns_rounds_to_closes_integer() -> None:
-    seconds = 0.000_000_000_9
-
-    nano_seconds = s2ns(seconds)
-
-    assert nano_seconds == 1
+    assert seconds == seconds_expected
--- tests/tests/utility/time/test_s2ns.py
+++ tests/tests/utility/time/test_s2ns.py
@@ -1,14 +1,26 @@
 from pytest import mark
 
-from aiotimer.utility.time import ns2s
+from aiotimer.utility.time import s2ns
 
 
-@mark.parametrize(('nanoseconds', 'seconds_expected'), [
-    (0, 0.0),
-    (1_000_000_000, 1.0),
-    (42_123_456_789, 42.123_456_789),
+@mark.parametrize(('seconds', 'nano_seconds_expected'), [
+    (0.0, 0),
+    (1.0, 1_000_000_000),
+    (42.123_456_789, 42_123_456_789),
 ])
-def test_s2ns(nanoseconds: int, seconds_expected: float) -> None:
-    seconds = ns2s(nanoseconds)
+def test_s2ns(seconds: float, nano_seconds_expected: int) -> None:
+    nano_seconds = s2ns(seconds)
 
-    assert seconds_expected == seconds
+    assert nano_seconds == nano_seconds_expected
+
+
+@mark.parametrize(('seconds', 'nano_seconds_expected'), [
+    (0.000_000_000_100, 0),
+    (0.000_000_000_400, 0),
+    (0.000_000_000_501, 1),
+    (0.000_000_000_900, 1),
+])
+def test_s2ns_rounds_to_closes_integer(seconds: float, nano_seconds_expected: int) -> None:
+    nano_seconds = s2ns(seconds)
+
+    assert nano_seconds == nano_seconds_expected
